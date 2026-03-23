9e36aa874f3ab23ee16fcc4c81db9cd1dd80905c
Skyost <me@skyost.eu>
2026-03-23 21:01:30 +0100

    fix: Fixed some problems with emails configuration.

--- app/email/mailer.ts
+++ app/email/mailer.ts
@@ -22,7 +22,11 @@ export abstract class Mailer {
   }
 
   public static async getBackendConfigMailer(): Promise<Mailer> {
-    let library = backendConfig.email.library
+    assert(!!backendConfig.authentication.providers.email.host, 'Missing email host.')
+    assert(!!backendConfig.authentication.providers.email.port, 'Missing email port.')
+    assert(!!backendConfig.authentication.providers.email.username, 'Missing email username.')
+    assert(!!backendConfig.authentication.providers.email.password, 'Missing email password.')
+    let library = backendConfig.authentication.providers.email.library
     if (library === 'auto') {
       library = process.env.NITRO_PRESET?.startsWith('cloudflare-') ? 'worker-mailer' : 'nodemailer'
     }
@@ -31,11 +35,11 @@ export abstract class Mailer {
         const mailer = await import('./nodemailer')
         return new mailer.NodemailerMailer(
           {
-            host: backendConfig.email.host,
-            port: backendConfig.email.port,
-            secure: backendConfig.email.secure,
-            username: backendConfig.email.username,
-            password: backendConfig.email.password,
+            host: backendConfig.authentication.providers.email.host,
+            port: backendConfig.authentication.providers.email.port,
+            secure: backendConfig.authentication.providers.email.secure ?? true,
+            username: backendConfig.authentication.providers.email.username,
+            password: backendConfig.authentication.providers.email.password,
           },
         )
       }
@@ -43,11 +47,11 @@ export abstract class Mailer {
         const mailer = await import('./workermailer')
         return new mailer.CloudflareWorkerMailer(
           {
-            host: backendConfig.email.host,
-            port: backendConfig.email.port,
-            secure: backendConfig.email.secure,
-            username: backendConfig.email.username,
-            password: backendConfig.email.password,
+            host: backendConfig.authentication.providers.email.host,
+            port: backendConfig.authentication.providers.email.port,
+            secure: backendConfig.authentication.providers.email.secure ?? true,
+            username: backendConfig.authentication.providers.email.username,
+            password: backendConfig.authentication.providers.email.password,
           },
         )
       }
