0eb785719e89daeb507734d7a18b55d7436dcd89
Skyost <me@skyost.eu>
2026-04-03 23:47:49 +0200

    fix: Disabled cache in callback and redirect routes.

--- routes/auth/provider/[provider]/redirect.get.ts
+++ routes/auth/provider/[provider]/redirect.get.ts
@@ -4,6 +4,8 @@ import { defineHandler, type H3Event } from 'nitro/h3'
 export default defineHandler({
   middleware: [rateLimit()],
   handler: async (event: H3Event) => {
+    event.res.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate')
+    event.res.headers.set('Pragma', 'no-cache')
     const authProvider = useAuthProvider(event)
     return redirectIntoApp((await authProvider.redirect(event as AppEvent)).toString())
   },
