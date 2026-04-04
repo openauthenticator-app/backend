889137c8e888f48e157cee1124dc71bb0e1594c8
Skyost <me@skyost.eu>
2026-04-04 13:45:35 +0200

    chore: Updated cache headers in callbacks.

--- routes/auth/provider/[provider]/redirect.get.ts
+++ routes/auth/provider/[provider]/redirect.get.ts
@@ -4,8 +4,9 @@ import { defineHandler, type H3Event } from 'nitro/h3'
 export default defineHandler({
   middleware: [rateLimit()],
   handler: async (event: H3Event) => {
-    event.res.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate')
+    event.res.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
     event.res.headers.set('Pragma', 'no-cache')
+    event.res.headers.set('Expires', '0')
     const authProvider = useAuthProvider(event)
     return redirectIntoApp((await authProvider.redirect(event as AppEvent)).toString())
   },
