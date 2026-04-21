2f2ef97bc998bc380f9e3b86f5437b433c4f769d
Skyost <me@skyost.eu>
2026-04-21 11:08:34 +0200

    chore: Updated the admin routes.

---
+++ routes/admin/prune/sessions.post.ts
@@ -0,0 +1,7 @@
+import { Session } from '~/app'
+import { defineHandler } from 'nitro/h3'
+
+export default defineHandler(async () => {
+  await Session.pruneExpired()
+  return SuccessObject.fromData().toResponse()
+})
---
+++ routes/admin/prune/totps.post.ts
@@ -0,0 +1,8 @@
+import { TotpBucket } from '~/app'
+import { defineHandler, type H3Event, readBody } from 'nitro/h3'
+
+export default defineHandler(async (event: H3Event) => {
+  const body = await readBody<{ days: number | undefined }>(event)
+  await TotpBucket.pruneDeletedTotps(body?.days)
+  return SuccessObject.fromData().toResponse()
+})
