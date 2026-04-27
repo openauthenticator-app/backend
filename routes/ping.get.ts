d110f430a8a4e5ab01e3a07995d1b051ac1e5f79
Skyost <me@skyost.eu>
2026-04-27 19:19:11 +0200

    fix: Fixed a bug with the `/ping` endpoint.

--- routes/ping.get.ts
+++ routes/ping.get.ts
@@ -2,5 +2,5 @@ import { defineHandler } from 'nitro/h3'
 
 export default defineHandler({
   middleware: [rateLimit()],
-  handler: SuccessObject.fromData().toResponse,
+  handler: event => SuccessObject.fromData().toResponse(event),
 })
